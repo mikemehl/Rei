e10665200896e2e40b406d6f29b8fde3b721ce7f
mikemehl <michaelalanmehl@gmail.com>
2021-05-24 11:02:22 -0400

    Some refactoring.

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,12 @@ async fn main() {
     println!("Rei: A Line Mode Gemini Browser");
     let mut cont = true;
     while cont {
-        if let Ok(p) = prompt().await {
-            continue;
-        } else {
-            cont = false;
-        }
+        if let Ok(p) = prompt() {
+            if(execute_command(p).await) {
+                continue;
+            } 
+        }     
+        cont = false;
     }
 }
 
@@ -40,35 +41,61 @@ async fn page_test() {
 }
 
 // Functions for user interaction.
-async fn prompt() -> Result<ParseResponse, String> {
+fn prompt() -> Result<ParseResponse, String> {
     print!("* ");
     let _ = std::io::stdout().flush();
     let mut response = String::new();
     let bytes_read = std::io::stdin().read_line(&mut response).unwrap();
-    return parse_response(response).await;
+    return  parse_response(response);
 }
 
 enum ParseResponse {
     Document(String),
+    GoUrl(url::Url),
+    SearchBackwards(String),
+    SearchForwards(String),
+    GoBack,
+    GoForward,
+    Print(u32, u32),
+    Enumerate(u32, u32),
+    Page(u32),
+    History(u32),
+    Quit,
 }
-async fn parse_response(resp : String) -> Result<ParseResponse, String> {
+fn parse_response(resp : String) -> Result<ParseResponse, String> {
     if resp.len() < 2 {
        return Err("SHORT RESPONSE".to_string()); 
     }
 
     let mut tokens = resp.split(" ");
-    if tokens.next() == Some("g") {
-        if let Some(url) = tokens.next() {
-            if let Ok(page) = get_page(url).await {
-                if let Some(body) = page.body {
-                    println!("{}", body);
-                    return Ok(ParseResponse::Document(body));
+    let cmd = tokens.next();
+    match cmd {
+        Some("g") => {
+            if let Some(url) = tokens.next() {
+                if let Ok(url) = url::Url::parse(url) {
+                    return Ok(ParseResponse::GoUrl(url));
                 }
             }
-        }
+        },
+        Some("q") => return Ok(ParseResponse::Quit),
+        _ => return Err("Unknown command.".to_string()),
     }
-
     println!("OH NO YOU GOOFED");
 
     return Err("Unable to parse response.".to_string());
+}
+
+async fn execute_command(cmd : ParseResponse) -> bool {
+    match cmd {
+        ParseResponse::GoUrl(url) => {
+            if let Ok(page) = gemini_fetch::Page::fetch(&url, None).await {
+                if let Some(body) = page.body {
+                    println!("{}", body);
+                }
+            }
+        },
+        ParseResponse::Quit => return false,
+        _ => println!("NOT YET IMPLEMENTED"),
+    }
+    return true;
 }
